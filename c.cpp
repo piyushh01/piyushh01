c66ffa9c91f73d901ad6949e25bbdee30b2e4f55
Student ATHARVA GAIKWAD <atharvagaikwad2022.ainds@mmcoe.edu.in>
2024-03-12 20:39:47 +0530

    "for loop insert"

--- c.cpp
+++ c.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(int argc, char const *argv[])
+{   for (int i = 0; i < 6; i++)
 {
-    cout<<"Hello World";
+      cout<<"Hello World";
+}
+
+  
     return 0;
 }
-f vjbfibilgfb
